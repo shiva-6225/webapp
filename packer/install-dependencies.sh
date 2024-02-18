bebebd93348d2c998ada030ed5cd617d532923aa
Shiva Teja <dasi.s@northeastern.edu>
2024-02-17 21:28:00 -0500

    added packer validation workflow

--- packer/install-dependencies.sh
+++ packer/install-dependencies.sh
@@ -7,18 +7,6 @@ sudo yum update -y
 # Install MySQL server
 sudo yum install -y mysql-server
 
-# Start MySQL service
-sudo systemctl start mysqld
-
-# Enable MySQL to start on boot
-sudo systemctl enable mysqld
-
-# Set MySQL root password
-sudo mysql -e "ALTER USER ${MYSQL_USERNAME}@${MYSQL_SERVER_URL} IDENTIFIED BY '${MYSQL_PASSWORD}';"
-
-# Create database named "CSYE"
-sudo mysql -u "${MYSQL_USERNAME}" -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS CSYE6225;"
-
 # Install Node and npm
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
 sudo yum install -y nodejs
