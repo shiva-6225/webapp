eef014ac30f77535ea15804859a55b4c42dea81b
Shiva Teja <dasi.s@northeastern.edu>
2024-02-21 17:53:30 -0500

    updated workflow

--- setupApp.sh
+++ setupApp.sh
@@ -24,8 +24,8 @@ sudo rm -f webapp.zip
 
 sudo groupadd -r csye6225 || true
 sudo useradd -r -g csye6225 -s /usr/sbin/nologin csye6225 || true
-sudo chmod -R 755 /home/packer/webapp
-sudo chown -R csye6225:csye6225 /home/packer/webapp
+sudo chmod -R 755 /home/packer
+sudo chown -R csye6225:csye6225 /home/packer
 
 
 sudo mv /tmp/csye6225.service /etc/systemd/system/csye6225.service
