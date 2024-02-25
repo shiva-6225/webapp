b9665a17d65d8b6b825ebad14f5599e9b3bfb38c
Shiva Teja <dasi.s@northeastern.edu>
2024-02-25 10:43:23 -0500

    removed local db startup here

--- setupApp.sh
+++ setupApp.sh
@@ -8,17 +8,6 @@ cd /home/packer/ && sudo unzip webapp.zip
 
 cd /home/packer/webapp && sudo npm install
 
-ENV_FILE="/home/packer/webapp/.env"
-
-
-sudo tee "$ENV_FILE" > /dev/null <<EOF
-DB_NAME=${MYSQL_DATABASE}
-DB_USER=${MYSQL_USERNAME}
-DB_PASSWORD=${MYSQL_PASSWORD}
-DB_HOST=${MYSQL_SERVER_URL}
-DB_PORT=${MYSQL_PORT}
-EOF
-
 
 sudo rm -f webapp.zip
 
@@ -26,8 +15,6 @@ sudo groupadd -r csye6225 || true
 sudo useradd -r -g csye6225 -s /usr/sbin/nologin csye6225 || true
 sudo chmod -R 755 /home/packer
 sudo chown -R csye6225:csye6225 /home/packer
-
-
 sudo mv /tmp/csye6225.service /etc/systemd/system/csye6225.service
 
 
