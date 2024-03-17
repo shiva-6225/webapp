80e0dc2f8fe2338fb3e9100e5e99d2991757fbc3
Shiva Teja <dasi.s@northeastern.edu>
2024-03-17 14:32:04 -0400

    fixed user issue

--- restartOpsAgent.sh
+++ restartOpsAgent.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+sudo -i
+cd ..
+cd ..
+cd ..
 sudo mv /tmp/ops-agent-config.yaml /etc/google-cloud-ops-agent/config.yaml
+sudo groupadd -r csye6225 || true
+sudo useradd -r -g csye6225 -s /usr/sbin/nologin csye6225 || true
 sudo chown -R csye6225:csye6225 /etc/google-cloud-ops-agent/config.yaml
 sudo chmod 600 /etc/google-cloud-ops-agent/config.yaml
 echo 'Restarting Ops Agent to apply new configuration...'
