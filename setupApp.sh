8f28456e307220086bc9f21e2a7207f7bc05edc2
Shiva Teja <dasi.s@northeastern.edu>
2024-03-17 14:32:04 -0400

    fixed user issue

--- setupApp.sh
+++ setupApp.sh
@@ -10,9 +10,6 @@ cd /home/packer/webapp && sudo npm install
 
 
 sudo rm -f webapp.zip
-
-sudo groupadd -r csye6225 || true
-sudo useradd -r -g csye6225 -s /usr/sbin/nologin csye6225 || true
 sudo chmod -R 755 /home/packer
 sudo chown -R csye6225:csye6225 /home/packer
 sudo mv /tmp/csye6225.service /etc/systemd/system/csye6225.service
