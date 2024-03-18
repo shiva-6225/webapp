348c4a2ed73a32ddef280f196bf0b5642776e764
Shiva Teja <dasi.s@northeastern.edu>
2024-03-17 20:54:37 -0400

    restart command corrected

--- restartOpsAgent.sh
+++ restartOpsAgent.sh
@@ -9,5 +9,6 @@ sudo useradd -r -g csye6225 -s /usr/sbin/nologin csye6225 || true
 sudo chown -R csye6225:csye6225 /etc/google-cloud-ops-agent/config.yaml
 sudo chmod 600 /etc/google-cloud-ops-agent/config.yaml
 echo 'Restarting Ops Agent to apply new configuration...'
-sudo systemctl restart google-cloud-ops-agent.target
+sudo systemctl status google-cloud-ops-agent*.service
+sudo systemctl restart google-cloud-ops-agent.service
 echo 'Ops Agent is now monitoring application logs.'
