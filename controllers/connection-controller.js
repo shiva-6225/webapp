d68ae1104b892f8ea92ecfd0fc7c6900e0f69276
Shiva Teja <dasi.s@northeastern.edu>
2024-03-18 14:54:26 -0400

    added loggers

--- controllers/connection-controller.js
+++ controllers/connection-controller.js
@@ -1,12 +1,16 @@
 const sequelize = require("../db-connection.js");
 const { setResponse, setErrorResponse } = require("./response-handler.js");
-
+const Logger = require('node-json-logger');
+const logger = new Logger();
 exports.connectDB = async (req, res) => {
+    logger.info("Attempting to connect to the database...");
     try {
         await sequelize.authenticate();
+        logger.info("Database connection was successful.");
         setResponse(req, res);
     }
     catch (err) {
+        logger.error("Database connection failed", { error: err.message });
         setErrorResponse(err, res);
     }
 }
--- services/user-service.js
+++ services/user-service.js
@@ -1,8 +1,10 @@
 const User = require("../models/user.js");
 const bcrypt = require('bcrypt');
-
+const Logger = require('node-json-logger');
+const logger = new Logger();
 // function to fetch the user chat
 exports.register = async (user) => {
+    logger.info("Attempting to register a new user", { username: user.username });
     const { firstname, lastname, username, password } = user;
     const saltRounds = 10;
     const hashedPassword = await bcrypt.hash(password, saltRounds);
@@ -16,20 +18,25 @@ exports.register = async (user) => {
 };
 
 exports.fetchUser = async (username, password) => {
+    logger.info("Attempting to fetch user", { username });
     const user = await User.findOne({ where: { username } });
     if (user) {
         const passwordMatched = await bcrypt.compare(password, user.password);
         if (passwordMatched) {
+            logger.info("Password matched for user", { username });
             return { success: true, user };
         } else {
+            logger.warn("Incorrect password for user", { username });
             return { success: false, message: 'Incorrect password' };
         }
     } else {
+        logger.warn("User not found during fetch", { username });
         return { success: false, message: 'User not found' };
     }
 };
 
 exports.updateUser = async (username, oldPassword, userDetails) => {
+    logger.info("Attempting to update user", { username });
     const { firstname, lastname, password } = userDetails;
     const user = await User.findOne({ where: { username } });
     if (user) {
@@ -38,11 +45,14 @@ exports.updateUser = async (username, oldPassword, userDetails) => {
             const saltRounds = 10;
             const hashedPassword = await bcrypt.hash(password, saltRounds);
             await User.update({ first_name: firstname, last_name: lastname, password: hashedPassword, account_updated: Date.now() }, { where: { username } });
+            logger.info("User updated successfully", { username });
             return { status: 204 };
         } else {
+            logger.warn("Incorrect old password for user update", { username });
             return { status: 401 };
         }
     } else {
+        logger.warn("User not found during update", { username });
         return { status: 404 };
     }
 };
