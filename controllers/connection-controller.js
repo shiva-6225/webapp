c53718822a9d0a2c4f2500b40cd1ce23674c3647
Shiva Teja <dasi.s@northeastern.edu>
2024-03-19 10:30:48 -0400

    winston logger added

--- controllers/connection-controller.js
+++ controllers/connection-controller.js
@@ -8,7 +8,7 @@ const logger = winston.createLogger({
   format: combine(timestamp(), json()),
   transports: [
     new winston.transports.File({
-      filename: '/var/log/webapp/test.log',
+      filename: 'webapp.log',
     }),
   ],
 });
