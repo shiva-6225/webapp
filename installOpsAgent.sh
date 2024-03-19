a086285badf02bece8137355a379e4a5decc4ced
Shiva Teja <dasi.s@northeastern.edu>
2024-03-18 21:35:13 -0400

    auth service with ops agent

--- installOpsAgent.sh
+++ installOpsAgent.sh
@@ -5,3 +5,11 @@ echo 'Installing Ops Agent...'
 curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 echo 'Ops Agent installation completed.'
+cd /
+sudo mkdir -p /var/log/webapp/
+sudo touch /var/log/webapp/test.log
+sudo chmod -R go+r /var/log/webapp/
+
+sudo mkdir -p /var/log/webapp/
+sudo touch /var/log/webapp/test.log
+sudo chmod -R go+r /var/log/webapp/
